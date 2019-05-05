5088ffebe5c728a12a292e745d6490be2c004244
Matthew Niznik <matt@matthewniznik.com>
2019-05-05 09:50:24 -0500

    Update sample script

--- makeNizPainting.py
+++ makeNizPainting.py
@@ -2,13 +2,16 @@ import nizpaint as nizp
 from PIL import Image
 import random
 
-xPixels = 1024
-yPixels = 1024
+xPixels = 1600
+yPixels = 900
 
-img = Image.new('RGB',(xPixels,yPixels),'black')
+def getRandom255():
+    return (int(random.random()*255))
+
+img = Image.new('RGB',(xPixels,yPixels),(getRandom255(),getRandom255(),getRandom255()))
 
 for ii in range(0,1500):
-    color = (int(random.random()*255),int(random.random()*255),int(random.random()*255))
+    color = (getRandom255(),getRandom255(),getRandom255())
     radius = int(random.random()*50)
     x = int(random.random()*xPixels)
     y = int(random.random()*yPixels)
